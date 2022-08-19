f5bfe6d77a38702e9d4aaefbefe5d6453c8f9c66
嘻嘻心木 <1358419622@qq.com>
2022-08-19 17:43:12 +0800

    头像预览

--- src/views/user-avatar/index.vue
+++ src/views/user-avatar/index.vue
@@ -3,9 +3,8 @@
     <div slot="header" class="clearfix">
       <span>更换头像</span>
     </div>
-    <div class="the_img">
-      <img src="@/assets/avatar.jpg" />
-    </div>
+    <img class="the_img" src="@/assets/avatar.jpg" v-if="!newAvatar" />
+    <img class="the_img" :src="newAvatar" v-else />
     <div class="btn-box">
       <input
         type="file"
@@ -28,10 +27,16 @@
 <script>
 export default {
   name: "user-avatar",
+  data() {
+    return {
+      newAvatar: "",
+    };
+  },
   methods: {
     selectAvatarFn() {
       //目的:为了让input[type=file]标签，让他再用S代码来触发它的点击事件(导致它默认行为给一个文件选择窗口)
-      //原因:input[type=file]它是原生标签,样式不太好改//解决:移花接木
+      //原因:input[type=file]它是原生标签,样式不太好改
+      //解决:移花接木
       this.$refs.iptRef.click();
     },
     updateAvatarFn() {},
@@ -42,6 +47,27 @@ export default {
       } else {
         console.log(file[0]);
         //证明它选择了文件(默认只能选1个，如果选择多个你需要给input标签加额外原生属性)
+
+        // img标签的src值
+        //*只能是图片的"链接地址"(外链http://开头，图片文件相对路径)
+        //*或者是图片的base64字符串(而且字符串还必须是data:image/ png;base64，图片转base64字符串)
+
+        //解决方案1:文件 ->内存临时地址(这个地址只能在js里内存里不能发给后台)
+        //语法:URL.createObjectURL(文件)
+        //返回值:内存临时地址
+        // this.newAvatar = URL.createObjectURL(file[0]);
+
+        //解决方案2:文件 -> base64字符串(此字符串是可以发给后台的)
+
+        // 1. 创建 FileReader 对象
+        const fr = new FileReader();
+        // 2. 调用 readAsDataURL 函数，读取文件内容
+        fr.readAsDataURL(files[0]);
+        // 3. 监听 fr 的 onload 事件
+        fr.onload = (e) => {
+          // 4. 通过 e.target.result 获取到读取的结果，值是字符串（base64 格式的字符串）
+          this.newAvatar = e.target.result;
+        };
       }
     },
   },
