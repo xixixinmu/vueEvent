be3aa24e35ec0ae7b17e0651653270e0f28d72a4
嘻嘻心木 <1358419622@qq.com>
2022-08-25 23:45:43 +0800

    开发环境不使用CDN

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 // 需要排除的包对象
 let externals = {}
+let CDN={css:[],js:[]}
 // 判断是否是生产环境
 const isProduction = process.env.NODE_ENV === 'production'
 // 如何是生产环境，需要执行以下逻辑
@@ -24,6 +25,24 @@ if (isProduction) {
     'element-ui': 'ELEMENT',
     'vue-quill-editor': 'VueQuillEditor',
     'vuex-persistedstate': 'createPersistedState'
+  },
+  CDN={
+    css:['https://unpkg.com/element-ui@2.15.8/lib/theme-chalk/index.css',
+  'https://unpkg.com/quill@1.3.7/dist/quill.core.css',
+  'https://unpkg.com/quill@1.3.7/dist/quill.snow.css',
+  'https://unpkg.com/quill@1.3.7/dist/quill.bubble.css',],
+  js:[
+    'https://unpkg.com/echarts@5.3.2/dist/echarts.min.js',
+    'https://unpkg.com/vue@2.6.14/dist/vue.js',
+    'https://unpkg.com/vue-router@3.5.1/dist/vue-router.js',
+    'https://unpkg.com/vuex@3.6.2/dist/vuex.js',
+    'https://unpkg.com/axios@0.27.2/dist/axios.min.js',
+    'https://unpkg.com/dayjs@1.11.3/dayjs.min.js',
+    'https://unpkg.com/element-ui@2.15.8/lib/index.js',
+    'https://unpkg.com/quill@1.3.7/dist/quill.js',
+    'https://unpkg.com/vue-quill-editor@3.0.6/dist/vue-quill-editor.js',
+    'https://unpkg.com/vuex-persistedstate@3.2.1/dist/vuex-persistedstate.umd.js'
+  ]
   }
 }
 module.exports = defineConfig({
@@ -40,7 +59,7 @@ module.exports = defineConfig({
   //如果你敲击yarn server,process.env.NODE_ENV的值就是'development'字符串
   //如果你敲击yarn build , process.env.NODE_ENV的值就是'production'字符串
 
-  publicPath: process.env.NODE_ENV==="production"?'./':"/",
+  publicPath: process.env.NODE_ENV==="production"?'./':"/",//这句话是下面总结一要加的，其他比初始多的代码是总结二
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
@@ -51,5 +70,23 @@ module.exports = defineConfig({
     //     '@': resolve('src')
     //   }
     // }
+  },
+  chainWebpack(config){
+    config.plugin("html").tap(args=>{
+      args[0].cdn=CDN
+      return args
+    })
   }
 });
+
+//总结:
+// 1. publicPath:影响的是打包(webpack开发服务器/dist)的时候index.html引入其他资源的前缀地址
+//情况1:开发环境，值:'/'
+//情况2:生产环境，值:'./'(才能确保dist/index.html相对路径下访问)
+// 2. dist瘦身:影响dist的体积，其他第三方包用cdn地址引入到index.html中保证运行
+//情况1:开发环境
+//(1): externals无值(不排除第三方包)
+//(2): index.html里不引入cdn地址
+//情况2:生产环境
+//(1): externals有值(排除第三方包)
+//(2): index.html引入cdn地址
