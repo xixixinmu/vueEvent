3fbb0e5720cffe4844b7fd3d54dc9d837a20921b
嘻嘻心木 <1358419622@qq.com>
2022-08-25 23:09:59 +0800

    开发环境不使用CDN

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,31 @@
 const { defineConfig } = require("@vue/cli-service");
+// 需要排除的包对象
+let externals = {}
+// 判断是否是生产环境
+const isProduction = process.env.NODE_ENV === 'production'
+// 如何是生产环境，需要执行以下逻辑
+if (isProduction) {
+  externals = {
+    /**
+      * externals 对象属性解析：
+      * '包名': '在项目中引入的名字'
+      * 以 element-ui 举例 我再 main.js 里是以
+      * import ELEMENT from 'element-ui'
+      * Vue.use(ELEMENT)
+      * 这样引入的，所以我的 externals 的属性值应该是 ELEMENT
+      * 一定要去main.js设置
+    */
+    'echarts': 'echarts',
+    'vue': 'Vue',
+    'vue-router': 'VueRouter',
+    'vuex': 'Vuex',
+    'axios': 'axios',
+    'dayjs': 'dayjs',
+    'element-ui': 'ELEMENT',
+    'vue-quill-editor': 'VueQuillEditor',
+    'vuex-persistedstate': 'createPersistedState'
+  }
+}
 module.exports = defineConfig({
   transpileDependencies: true,
   //影响打包时,index.html引入其他资源的前缀地址
@@ -18,19 +45,7 @@ module.exports = defineConfig({
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
     // name: name,
-    externals: {
-      // 基本格式：
-      // '包名' : '在项目中引入的名字'
-      'echarts': 'echarts',
-      'vue': 'Vue',
-      'vue-router': 'VueRouter',
-      'vuex': 'Vuex',
-      'axios': 'axios',
-      'dayjs': 'dayjs',
-      'element-ui': 'ELEMENT',
-      'vue-quill-editor': 'VueQuillEditor',
-      'vuex-persistedstate': 'createPersistedState'
-    },
+    externals: externals
     // resolve: {
     //   alias: {
     //     '@': resolve('src')
